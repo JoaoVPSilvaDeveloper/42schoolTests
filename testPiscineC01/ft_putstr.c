cc978c0016ead3b1d9226011278f7ec5f1628a8b
JoaoVPSilvaDeveloper <joaovpsilvadeveloper@gmail.com>
2025-03-02 00:53:03 +0000

    ok

--- testPiscineC01/ft_putstr.c
+++ testPiscineC01/ft_putstr.c
@@ -2,17 +2,14 @@
 
 void ft_putstr(char *str)
 {
-    int i = 0;
-
-    while(str[i] != '\0')
+    while(*str != '\0')
       {
-          write(1, &str[i], 1);
-          i++;
+          write(1, str, 1);
+          str++;
       }
 }
-/*
+
 int main()
 {
   ft_putstr("Hello World!\n");
 }
- */
