68d58bc99eeb0d0b38d0af6aec273bc11e7b5c94
JoaoVPSilvaDeveloper <joaovpsilvadeveloper@gmail.com>
2025-03-05 19:47:03 +0000

    Ok

--- testPiscineC02/ft_str_is_numeric.c
+++ testPiscineC02/ft_str_is_numeric.c
@@ -1,10 +1,9 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdbool.h>
-
-int    ft_str_is_numeric(char *str)
+/*
+int    ft_str_is_numeric0(char *str)
 {
   int limite;
+
   for (limite = 0;  str[limite] != '\0'; limite++)
   {
     if (!isdigit(str[limite]))
@@ -14,12 +13,25 @@ int    ft_str_is_numeric(char *str)
   }
   return true;
 }
+*/
+
+int    ft_str_is_numeric(char *str)
+{
+  int limite;
+
+  for (limite = 0;  str[limite] != '\0'; limite++) {
+    if (str[limite] < '0' || str[limite] > '9') {
+      return 0;
+      }
+  }
+  return 1;
+}
 
 int  main(void)
 {
-  char str[] = "0123456789W";
+  char str[] = "123456789";
 
-  if (ft_str_is_numeric(str) == true)
+  if (ft_str_is_numeric(str))
     printf("true\n");
   else
     printf("false\n");
