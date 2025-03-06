eca3fa16068d9833062ff98fef8bb780ea37aaae
JoaoVPSilvaDeveloper <joaovpsilvadeveloper@gmail.com>
2025-03-06 22:47:40 +0000

    Ok

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int ft_str_is_numeric(char *str)
+{
+  int limite;
+
+  limite = 0;
+  if (str[limite] == '\0')
+  {
+    return (1);
+  }
+  while (str[limite] != '\0')
+  {
+    if (!(str[limite] >= '0' && str[limite] <= '9'))
+      return (0);
+      
+    limite++;
+  }
+  return (1);
+}
+/*
+int  main(void)
+{
+  char str[] = "123456789";
+
+  if (ft_str_is_numeric(str))
+    printf("true\n");
+  else
+    printf("false\n");
+}*/
